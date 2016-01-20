8fe107fa6735f511a438d60cfcbc5020dd14aad2
Ratnadeep Debnath <rtnpro@gmail.com>
2016-01-20 16:13:26 +0530

    Generate runtime MOTD using motdgen.py.
    It looks for executable scripts inside a specified directory(/etc/motdgen.d/, by default)
    and executes all scripts alphabetically, concatenates the output and prints to
    stdout.

---
+++ motdgen.d/02-updateinfo.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /var/run/dnf-updateinfo.txt
---
+++ motdgen.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import subprocess
+
+
+def run_script(path):
+    ret = subprocess.Popen(
+        path, shell=True, stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    out, err = ret.communicate()
+    return out
+
+
+def run_scripts(path):
+    output = ""
+    for item in sorted(os.listdir(path)):
+        item_path = os.path.join(path, item)
+        if os.path.isfile(item_path) and os.access(item_path, os.X_OK):
+            output += run_script(item_path)
+    return output
+
+
+def generate_motd(source='/etc/motdgen.d/', outfile=None):
+    message = run_scripts(source)
+    if outfile:
+        with open(outfile, 'w') as f:
+            f.write(message)
+    else:
+        print message
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s',
+                        '--source',
+                        help='Directory containing scripts to generate MOTD',
+                        dest='source',
+                        default='/etc/motdgen.d/')
+    parser.add_argument('-o',
+                        '--output',
+                        help='File to dump generated MOTD',
+                        dest='dest',
+                        default=None)
+    args = parser.parse_args()
+    generate_motd(args.source, args.dest)
+
+
+if __name__ == '__main__':
+    cli()
