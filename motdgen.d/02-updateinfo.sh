1a6be65898a3db9037264b07aee72451bc086508
Dusty Mabe <dusty@dustymabe.com>
2016-03-10 11:18:09 -0500

    Improve message output when no updateinfo available
    Let the user know how they can find updateinfo if they are curious.

--- motdgen.d/02-updateinfo.sh
+++ motdgen.d/02-updateinfo.sh
@@ -2,5 +2,5 @@ path=/var/run/updateinfo.txt
 if [ -f "$path" ]; then
     cat $path
 else
-    echo "Update info not available!"
+    echo "Update info not available yet! Run 'dnf updateinfo' for info."
 fi
